7f8242a45fb3dda8e8d1bccb53cd85f2ef8e5cc0
Igor <denlandpud@gmail.com>
2023-02-25 15:06:37 +0300

    task_58

---
+++ task_58/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить
+// произведение двух матриц. Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+using System; 
+using static System.Console;
+Clear();
+
+Write("enter the number of rows of the 1 array: ");
+int rowsA = int.Parse(ReadLine());
+Write("enter the number of columns of the 2 array: ");
+int columnsA = int.Parse(ReadLine());
+Write("enter the number of rows of the 1 array: ");
+int rowsB = int.Parse(ReadLine());
+Write("enter the number of columns of the 2 array: ");
+int columnsB = int.Parse(ReadLine());
+if (columnsA != rowsB)
+{
+    WriteLine("Such matrices cannot nultiplied!!!");
+    return;
+}
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+WriteLine();
+PrintArray(B);
+WriteLine();
+PrintArray(GetMultiplicationMatrix(A, B));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
+
